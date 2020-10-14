e8c70570c692b53a3925999b80aafb22c8ce946b
LucasOsorio <etu36775@henallux.be>
2020-10-14 17:29:16 +0200

    creation bd

--- projetIA/connection/models.py
+++ projetIA/connection/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User_data(models.Model):
+    password=models.CharField(max_length=25)
+
+
+class Player(models.Model):
+    user=models.ForeignKey(User_data,on_delete=models.CASCADE)
+    username=models.CharField(max_length=25).primary_key=True
+    color1=models.IntegerField()
+    color2=models.IntegerField()
+    
+
+
--- projetIA/game/models.py
+++ projetIA/game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from models import Player
+
 
 # Create your models here.
+class Game_State(models.Model):
+    id=models.AutoField(primary_key=True)
+    current_player=models.IntegerField()
+    bord=models.CharField(max_length=146)
+class Game_Player(models.Model):
+    user_username=models.ForeignKey(Player,on_delete=models.CASCADE)
+    game_state_id=models.ForeignKey(Game_State,on_delete=models.CASCADE)
+    id=models.AutoField(primary_key=True)
+    pos=models.CharField(max_length=5)
+   
+
