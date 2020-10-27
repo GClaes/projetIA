e636968ba23e75bec172d4815408780009c054bc
GClaes <47348412+GClaes@users.noreply.github.com>
2020-10-27 15:16:53 +0100

    refactoring

---
+++ projetIA/game/business.py
@@ -0,0 +1,69 @@
+import ast
+
+def change_player(list_player,index_player):
+    index_player+=1
+    if(index_player==len(list_player)):
+        index_player=0
+    return index_player
+
+    
+def calculate_position(player_pos, movement):
+    position =  [player_pos[0]+movement[0], player_pos[1]+movement[1]]
+    for pos in position:
+        if pos < 0 or pos > 7:
+            #Raise error
+            return player_pos
+
+    return position
+
+def update_board_content(board, player, num_player):
+    position = player.pos
+    board[position[0]][position[1]] = num_player+1
+    return board
+
+def string_to_list(s):
+    return ast.literal_eval(s)
+
+def listing_game_players(game_players):
+    game_players = list(game_players)
+    game_players = listing_player_pos(game_players)
+    return game_players
+
+def listing_player_pos(players):
+    return [convert_pos(p) for p in players]
+
+def convert_pos(player):
+    player.pos = string_to_list(player.pos)
+    return player
+
+def build_game_state(game_state_data, players):
+
+    players = build_players_entities(players)
+
+    game_state = {
+        "game_id" : game_state_data.auto_increment_id,
+        "board" : game_state_data.board,
+        "players" : players,
+        "current_player" : 1,
+        "code" : 0, 
+    }
+
+    return game_state
+
+def build_players_entities(players):
+    players_obj = []
+    for player in players:
+        dic = {
+            "id":player.auto_increment_id,
+            "name" : player.user.username,
+            "color" : player.user.color1,
+            "position" : player.pos
+        }
+        players_obj.append(dic)
+
+    return players_obj
+
+def move_pos(player, movement, game_state, players):
+    player.pos = calculate_position(player.pos, movement)
+    game_state.board = update_board_content(game_state.board, player, players.index(player))
+    return game_state
--- projetIA/game/views.py
+++ projetIA/game/views.py
@@ -6,6 +6,8 @@ import random
 from game.models import Game_Player, Game_State
 from connection.models import User
 import ast
+from game.business import *
+from game.services import *
  
 
 class NewGameForm(forms.Form):
@@ -22,36 +24,20 @@ def index(request):
         form = NewGameForm(request.POST)
         if form.is_valid():
             board = [[1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,2]]
-            game_state1 = Game_State(current_player=1, board=board)
-            game_state1.save()
+            game_state_data = Game_State(current_player=1, board=board)
+            game_state_data.save()
 
             username1 = form.cleaned_data.get("player1")
             username2 = form.cleaned_data.get("player2")
             u1 = User.manager.get(username = username1)
             u2 = User.manager.get(username = username2)
-            game_player1 = Game_Player(user=u1, game_state=game_state1, pos=[0,0])
+            game_player1 = Game_Player(user=u1, game_state=game_state_data, pos=[0,0])
             game_player1.save()
-            game_player2 = Game_Player(user=u2, game_state=game_state1, pos=[7,7])
+            game_player2 = Game_Player(user=u2, game_state=game_state_data, pos=[7,7])
             game_player2.save()
 
-
-            game_state = { 
-                "game_id" : game_state1.auto_increment_id,
-                "board" : game_state1.board,
-                "players" : [{
-                        "id" :  game_player1.auto_increment_id,
-                        "name" : game_player1.user.username,
-                        "color" : "cyan",
-                        "position" : game_player1.pos
-                    },{
-                        "id" :  game_player2.auto_increment_id,
-                        "name" : game_player2.user.username,
-                        "color" : "orange",
-                        "position" : game_player2.pos
-                    }],
-                "current_player" : 0,
-                "code" : 0,
-            }
+            
+            game_state = build_game_state(game_state_data, [game_player1, game_player2])
 
             return render(request, 'game/new_game.html', game_state)
 
@@ -64,90 +50,24 @@ def apply_move(request) :
     game_id = rcontent.get("game_id")
 
     #Recupérer le game_state en DB
-    game_state_data = Game_State.manager.get(auto_increment_id=game_id)
-    game_state_data.board = ast.literal_eval(game_state_data.board)
-    game_players = Game_Player.manager.all().filter(game_state = game_state_data)
+    game_state_data = get_gamestate_data(game_id)
+    game_state_data.board = string_to_list(game_state_data.board)
+    game_players = get_all_player_from_gamestate(game_state_data)
+    game_players = listing_game_players(game_players)
+
+    #Changer
     game_player1 = game_players[0]
-    print(game_player1.auto_increment_id)
-    print(game_player1.pos)
-    game_player1.pos = ast.literal_eval(game_player1.pos)
     game_player2 = game_players[1]
- 
-
-
-    game_state = {
-        "game_id" : game_state_data.auto_increment_id,
-        "board" : game_state_data.board,
-        "players" : [{
-                "id" :  game_player1.auto_increment_id,
-                "name" : game_player1.user.username,
-                "color" : game_player1.user.color1,
-                "position" : game_player1.pos
-
-            },{
-                "id" :  20,
-                "name" : "Bob",
-                "color" : "orange",
-                "position" : [7,7]
-
-            }],
-        "current_player" : 1,
-        "code" : 0, 
-    }
-
+    #
     
-    #Le code sera à modifier pour le rendre plus fonctionnel paradigment parlant
-    #+Encore données brutes
-    player1 = game_state.get("players")[0]
-    pos_player1 = player1.get("position")
-    pos = calculate_position(pos_player1, movement)
-    game_state["players"][0]["position"] = pos
-    game_player1.pos = pos
+    game_state_data = move_pos(game_player1, movement, game_state_data, game_players)
+    game_state = build_game_state(game_state_data, game_players)
 
-    board = game_state.get("board")
-    players = game_state.get("players")
-    board = update_board_content(board, player1, players.index(player1))
-    game_state["board"]=board
 
-    #Sauver le game_state en DB
-    game_state_data.save()
-    game_player1.save()
+
+    #Persister les données
+    save_data(game_state_data)
+    save_data(game_player1)
 
 
     return JsonResponse(game_state)
-
-
-
-
-
-
-
-    #Changer Current_player
-    """
-    for player in game_players:
-        if player.auto_increment_id == p_player:
-            curr_player = game_players.index(player)
-    """
-"""
-def change_player(list_player,index_player):
-    if(index_player==len(list_player)):
-        index_player=0
-    else
-        index_player+=1
-    return index_player
-"""
-    
-#Rendre plus fonctionnel
-def calculate_position(player_pos, movement):
-    position =  [player_pos[0]+movement[0], player_pos[1]+movement[1]]
-    for pos in position:
-        if pos < 0 or pos > 7:
-            #Raise error
-            return player_pos
-
-    return position
-
-def update_board_content(board, player, index):
-    position = player.get("position")
-    board[position[0]][position[1]] = index+1
-    return board
